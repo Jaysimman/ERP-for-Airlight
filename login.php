9f04e53a7e8d28a51e3faba227a80095caa9ad2a
Jaysimman G <jaysimmannet@gmail.com>
2024-04-08 13:00:18 +0530

    Renaming and cleaning unwanted files

---
+++ login.php
@@ -0,0 +1,22 @@
+<?
+include 'libs/load.php';
+?>
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+ <head>
+    <script src="/airlighterp/assets/js/color-modes.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Airlight Naveen ERP Login page">
+    <meta name="author" content="Jaysimman">
+    <meta name="generator" content="Jaysimman ERP 1.0">
+    <title>Login</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+    <link rel="stylesheet" href="/airlighterp/assets/dist/css/bootstrap.min.css" >
+    <link rel="stylesheet" href="/airlighterp/css/login.css">
+  </head>
+  <body>
+    <?load_template('_login');?>
+  </body>
+</html>
