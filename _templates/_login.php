beaf61bae6e2dde0660a671dd0906d4e3ebfbb20
Jaysimman G <jaysimmannet@gmail.com>
2024-04-13 14:43:52 +0530

    changing signin -> login

--- _templates/_login.php
+++ _templates/_login.php
@@ -1,7 +1,7 @@
 <main class="form-signin w-100 m-auto">
 <form action="/airlighterp/enquiry.php" method="post">
 <img class="mb-4" src="https://airlight.in/wp-content/uploads/2017/09/Airlight-thinking-ahead-logo-1111111.png" alt="" width="72" height="57">
-<h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+<h1 class="h3 mb-3 fw-normal">Please Login</h1>
 <div class="form-floating">
 <input name="username" type="text" class="form-control" id="floatingInput" placeholder="name@example.com" required>
 <label for="floatingInput">
@@ -20,7 +20,7 @@ Password
 Remember me
 </label>
 </div> -->
-<button class="btn btn-primary w-100 py-2 hvr-grow" type="submit">Sign in</button>
+<button class="btn btn-primary w-100 py-2 hvr-grow" type="submit">Login</button>
 </form>
 </main>
 <script src="/airlighterp/assets/dist/js/bootstrap.bundle.min.js"></script>
