990503e3f432db0e4f494445788bff426952b413
Jaysimman G <jaysimmannet@gmail.com>
2024-04-13 15:22:28 +0530

    indent changes and path added in billform

--- _templates/_login.php
+++ _templates/_login.php
@@ -1,26 +1,22 @@
 <main class="form-signin w-100 m-auto">
-<form action="/airlighterp/enquiry.php" method="post">
-<img class="mb-4" src="https://airlight.in/wp-content/uploads/2017/09/Airlight-thinking-ahead-logo-1111111.png" alt="" width="72" height="57">
-<h1 class="h3 mb-3 fw-normal">Please Login</h1>
-<div class="form-floating">
-<input name="username" type="text" class="form-control" id="floatingInput" placeholder="name@example.com" required>
-<label for="floatingInput">
-Username
-</label>
-</div>
-<div class="form-floating">
-<input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
-<label for="floatingPassword">
-Password
-</label>
-</div>
-<!-- <div class="form-check text-start my-3">
-<input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
-<label class="form-check-label" for="flexCheckDefault">
-Remember me
-</label>
-</div> -->
-<button class="btn btn-primary w-100 py-2 hvr-grow" type="submit">Login</button>
-</form>
+    <form action="/airlighterp/enquiry.php" method="post">
+        <img class="mb-4" src="https://airlight.in/wp-content/uploads/2017/09/Airlight-thinking-ahead-logo-1111111.png" alt="" width="72" height="57">
+        <h1 class="h3 mb-3 fw-normal">Please Login</h1>
+        <div class="form-floating">
+        <input name="username" type="text" class="form-control" id="floatingInput" placeholder="name@example.com" required>
+        <label for="floatingInput">Username</label>
+        </div>
+        <div class="form-floating">
+        <input name="password" type="password" class="form-control" id="floatingPassword" placeholder="Password" required>
+        <label for="floatingPassword">Password</label>
+        </div>
+        <!-- <div class="form-check text-start my-3">
+        <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
+        <label class="form-check-label" for="flexCheckDefault">
+        Remember me
+        </label>
+        </div> -->
+        <button class="btn btn-primary w-100 py-2 hvr-grow" type="submit">Login</button>
+    </form>
 </main>
 <script src="/airlighterp/assets/dist/js/bootstrap.bundle.min.js"></script>
